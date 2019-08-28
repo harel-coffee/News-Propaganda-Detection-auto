3e23fea6aeb5c61a0ef46a7f8e0f336450beaabb
Somesh Singh <someshsingh9414@gmail.com>
2019-08-28 15:43:41 +0530

    Writer added

--- Datasets/TOI/scrape.py
+++ Datasets/TOI/scrape.py
@@ -1,4 +1,8 @@
 from newspaper import Article
+import csv
+import os.path
+
+filedir="REGION/{}.csv"
 
 def scrape_url(url):
     article = Article(url, language="en")
@@ -10,3 +14,14 @@ def scrape_url(url):
         "Text" : article.text,
         "Keywords" : article.keywords,
         "Summary" : article.summary }
+
+def append(region,dictionary):
+    csv_path=filedir.format(region)
+    print(csv_path)
+    if not os.path.exists(csv_path):
+        with open(csv_path, "w+") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=dictionary.keys())
+            writer.writeheader()
+    with open(csv_path, 'a+') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=dictionary.keys())
+        writer.writerow(dictionary)

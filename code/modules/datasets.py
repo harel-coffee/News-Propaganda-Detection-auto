475c3a4df8ade5856b57dedd7a77f44dc4b6d1bf
rajaswa <rajp4480@gmail.com>
2019-12-24 20:02:23 +0530

    Added FastText training notebook

--- code/modules/datasets.py
+++ code/modules/datasets.py
@@ -7,6 +7,9 @@ import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
 import os
 
+
+#ONE-HOT DATASET
+
 class OneHotTdLstmDataset(Dataset):
     def __init__(self, x_right_seqs, x_left_seqs, y, vocab_length, transform=None):
         self.x_right_seqs = x_right_seqs
@@ -36,4 +39,27 @@ class OneHotTdLstmDataset(Dataset):
         y_ = torch.tensor(self.y[idx])
         
         return x_r_one_hot, x_l_one_hot, y_
+    
         
+#FASTTEXT DATASET
+
+class FastTextTdLstmDataset(Dataset):
+    def __init__(self, x_right_seqs, x_left_seqs, y, transform=None):
+        self.x_right_seqs = x_right_seqs
+        self.x_left_seqs = x_left_seqs
+        self.y = y
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.x_right_seqs)
+    
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+            
+        x_r, x_l = self.x_right_seqs[idx], self.x_left_seqs[idx]
+        
+        x_r_ftxt, x_l_ftxt = torch.cat(x_r, dim=0), torch.cat(x_l, dim=0)
+        y_ = torch.tensor(self.y[idx])
+        
+        return x_r_ftxt, x_l_ftxt, y_

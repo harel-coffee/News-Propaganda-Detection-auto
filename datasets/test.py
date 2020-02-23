91199f5320cb5314a4d41664cd122d59d905e4b5
Somesh Singh <someshsingh9414@gmail.com>
2020-02-23 06:44:32 +0530

    Features added

---
+++ datasets/test.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import re
+import string
+from fuzzywuzzy import fuzz
+
+train=pd.read_csv('train.csv')
+Sentences=train['Sentence'].tolist()
+
+fuz=pd.DataFrame()
+
+def rep_c(df):
+  ret_df=pd.DataFrame()
+  for i,text in enumerate(Sentences):
+    ret_df[i]=df['Sentence'].apply(lambda x : fuzz.token_set_ratio(x.lower(),text.lower()))
+    if i%50==0:
+    	print(i)
+  return ret_df
+
+fuz=rep_c(train)
+train.to_csv('Fuzz_table.csv')
